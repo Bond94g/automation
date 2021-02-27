4e57496ea115caab73d82b78286e4a5e0140a097
Michael Evnin <m.evnin0608@mail.ru>
2021-02-27 22:15:56 +0500

    1 задание

---
+++ Less1 _1/src/MainClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass {
+
+    @Test
+    public void testGetLocalNumber() {
+        int localNumber = this.getLocalNumber();
+        Assert.assertTrue("значение должно быть равно 14", localNumber == 14);
+    }
+}
